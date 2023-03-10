53f4a34df6855b77436ee4de2ec4df4b66129d95
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-03-10 18:18:31 +0000

    game logic implemented

--- script.js
+++ script.js
@@ -3,27 +3,77 @@ let largeNumber = document.querySelector('.number');
 let score = document.querySelector('.score');
 let guess = document.querySelector('.guess');
 let check = document.querySelector('.check');
+let mainBody = document.querySelector('body');
+let againButton = document.querySelector('.again');
 
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-largeNumber.textContent = randomNumber;
+
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let currentScore = 20;
 
-check.addEventListener('click', function () {
+check.addEventListener("click", function () {
 
     guessNumber = Number(guess.value);
 
     if (!guessNumber) {
+
         myMessage.textContent = '⛔️ No number!';
+
+        // When guess is correct
     } else if (guessNumber === randomNumber) {
+
         myMessage.textContent = '🎉 Correct number!';
+        largeNumber.textContent = randomNumber;
+
+        mainBody.style.backgroundColor = '#60b347';
+        largeNumber.style.width = '30rem';
+
+
+        // When guess is too high
     } else if (guessNumber > randomNumber) {
-        myMessage.textContent = '⏫ Too high!';
-        currentScore--;
-        score.textContent = currentScore;
+
+        if (currentScore > 1) {
+
+            myMessage.textContent = '⏫ Too high!';
+            currentScore--;
+            score.textContent = currentScore;
+
+        } else {
+
+            myMessage.textContent = '💥 You lost!';
+            score.textContent = currentScore - 1;
+
+        }
+
+        // When guess is too low
     } else if (guessNumber < randomNumber) {
-        myMessage.textContent = '⏬ Too low!';
-        currentScore--;
-        score.textContent = currentScore;
+
+        if (currentScore > 1) {
+
+            myMessage.textContent = '⏬ Too low!';
+            currentScore--;
+            score.textContent = currentScore;
+
+        } else {
+
+            myMessage.textContent = '💥 You lost!';
+            score.textContent = currentScore - 1;
+
+        }
+
     }
+
+});
+
+againButton.addEventListener("click", function () {
+
+    currentScore = 20;
+    randomNumber = Math.trunc(Math.random() * 20) + 1;
+    myMessage.textContent = "Start guessing...";
+    largeNumber.textContent = "?";
+    guess.value = "";
+    score.textContent = currentScore;
+
+    mainBody.style.backgroundColor = '#222';
+    largeNumber.style.width = '15rem';
 
 });
